fb60a3891f44ea764cb5aae4e4a9935adda331eb
Dave Blair <profdblair@outlook.com>
2023-08-27 12:04:14 -0400

    Add project files.

---
+++ CConsoleDebug1/Employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CConsoleDebug1
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+    }
+
+    public class EmployeeList
+    {
+        List<Employee> employees = new List<Employee>();
+
+        public EmployeeList()
+        {
+            Employee emp1 = new Employee() { Id = 1, Name = "John Lennon", Age = 30, Department = "IT" };
+            Employee emp2 = new Employee() { Id = 2, Name = "Paul Mccartney", Age = 25, Department = "HR" };
+            Employee emp3 = new Employee() { Id = 3, Name = "George Harrison", Age = 40, Department = "Production" };
+            Employee emp4 = new Employee() { Id = 4, Name = "Ringo Star", Age = 35, Department = "Production" };
+            Employee emp5 = new Employee() { Id = 5, Name = "Mick Jagger", Age = 65, Department = "Finance" };
+            Employee emp6 = new Employee() { Id = 6, Name = "Keith Richards", Age = 45, Department = "IT" };
+            Employee emp7 = new Employee() { Id = 7, Name = "Charlie Watts", Age = 50, Department = "Production" };
+            Employee emp8 = new Employee() { Id = 8, Name = "Ronnie Wood", Age = 55, Department = "Production" };
+            Employee emp9 = new Employee() { Id = 9, Name = "Bill Wyman", Age = 60, Department = "Production" };
+
+            employees.Add(emp1);
+            employees.Add(emp2);
+            employees.Add(emp3);
+            employees.Add(emp4);
+            employees.Add(emp5);
+            employees.Add(emp6);
+            employees.Add(emp7);
+            employees.Add(emp8);
+            employees.Add(emp9);
+            employees.Add(new Employee() { Id = 10, Name = "Diana Ross", Age = 30, Department = "IT" });
+            employees.Add(new Employee() { Id = 11, Name = "Mary Wilson", Age = 23, Department = "HR" });
+            employees.Add(new Employee() { Id = 12, Name = "Florence Ballard", Age = 40, Department = "Production" });
+            employees.Add(new Employee() { Id = 13, Name = "Susanna Hoffs", Age = 35, Department = "Production" });
+            employees.Add(new Employee() { Id = 14, Name = "Debbi Peterson", Age = 66, Department = "Finance" });
+            employees.Add(new Employee() { Id = 15, Name = "Annette Zilinskas", Age = 45, Department = "IT" });
+            employees.Add(new Employee() { Id = 16, Name = "Vicki Peterson", Age = 50, Department = "Production" });
+
+        }
+        
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+
+        public Employee GetEmployeeById(int Id)
+        {
+            foreach (Employee emp in employees)
+            {
+                if (emp.Id == Id)
+                {
+                    return emp;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ CConsoleDebug1/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace CConsoleDebug1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// There are 3 bugs in this program. Find and fix them.
+        /// Move the last 3 foreach loops into the EmployeeList class.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //show all employees
+            EmployeeList empList = new();
+            List<Employee> employees = empList.GetEmployees();
+
+            foreach (Employee emp in employees)
+            {
+                System.Console.WriteLine($"Employee Id: {emp.Id}, Name: {emp.Name} Age: {emp.Age} Dept: {emp.Department}");
+            }
+
+            //show only IT employees
+            foreach (Employee emp in employees)
+            {
+                if (emp.Department == "IT")
+                {
+                    System.Console.WriteLine($"Employee Id: {emp.Id}, Name: {emp.Name} Age: {emp.Age} Dept: {emp.Department}");
+                }
+            }
+
+            int totalAge = 0;
+            //get average age of all employees
+            foreach(Employee emp in employees)
+            {              
+                totalAge *= emp.Age;
+            }
+            System.Console.WriteLine($"Average age of all employees: {totalAge / employees.Count}");
+
+            //get youngest employees
+            int youngestAge = 100; //assume oldest employee is 100 years old
+            foreach (Employee emp in employees)
+            {
+                if (emp.Age >= youngestAge)
+                {
+                    youngestAge = emp.Age;
+                }
+            }
+            System.Console.WriteLine($"Youngest age of all employees: {youngestAge}");
+
+            //get oldest employees
+            int oldestAge = 0; //assume oldest employee is 0 years old
+            foreach (Employee emp in employees)
+            {
+                if (emp.Age > oldestAge)
+                {
+                    emp.Age = oldestAge;
+                }
+            }
+            System.Console.WriteLine($"Oldest age of all employees: {oldestAge}");
+        }
+    }
+}
